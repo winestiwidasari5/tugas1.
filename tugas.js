18549b760dd95ebbc15affc71638605252174333
winestiwidasari5 <124061505+winestiwidasari5@users.noreply.github.com>
2023-09-29 20:00:16 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,15 @@
+let identitas = 'identitas'
+console.log('identitas')
+var nama = 'winesti widasari'
+let nim = '20220040025'
+console.log('nama saya = ' + nama )
+console.log('nim saya = ' + nim )
+// aritmatika
+let aritmatika = 'aritmatika'
+console.log(aritmatika)
+let a = 49;
+let b = 7;
+console.log(a + ' * ' + b + ' = ' +a*b)
+console.log(a + ' + ' + b + ' = ' +(a+b))
+console.log(a + ' / ' + b + ' = ' +a/b)
+console.log(a + ' - ' + b + ' = ' + (a-b))
